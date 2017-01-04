82d62304dc9917cc8c647706c7008459daa202b3
StephenRansom <stpransom@gmail.com>
2017-01-04 13:57:43 -0700

    setup actionbar with help/about added some vector icon resources

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,6 +2,8 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 //empubliteactivity
@@ -12,5 +14,23 @@ public class EmPubLiteActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.options, menu);
+        return(super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.about:
+                return(true);
+            case R.id.help:
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
