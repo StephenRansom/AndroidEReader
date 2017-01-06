92aec1f52442699cca19e5a368ab6adf1cb35520
StephenRansom <stpransom@gmail.com>
2017-01-06 15:44:04 -0700

    Setting up Contents Adapter and ViewPager

---
+++ app/src/main/java/com/commonsware/empublite/ContentsAdapter.java
@@ -0,0 +1,25 @@
+package com.commonsware.empublite;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.support.v13.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by XKDVD on 1/6/2017.
+ */
+
+public class ContentsAdapter extends FragmentStatePagerAdapter {
+    public ContentsAdapter(Activity ctxt){
+        super(ctxt.getFragmentManager());
+    }
+
+    @Override
+    public Fragment getItem(int arg0){
+        return null;
+    }
+
+    @Override
+    public int getCount(){
+        return 0;
+    }
+}
--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -3,18 +3,32 @@ package com.commonsware.empublite;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import io.karim.MaterialTabs;
 
 
 //empubliteactivity
 
 public class EmPubLiteActivity extends Activity {
 
+    private ViewPager pager;
+    private ContentsAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        //setting up ViewPager
+        pager=(ViewPager)findViewById(R.id.pager);
+        adapter= new ContentsAdapter(this);
+        pager.setAdapter(adapter);
+
+        MaterialTabs tabs=(MaterialTabs)findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
     }
 
 
